bb503b022801801fae4af6628053dbefd90b065a
mlahiru1 <164457106+mlahiru1@users.noreply.github.com>
2024-08-20 17:47:34 +0530

    first Set

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUser")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "User Updated!";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+        return "User Deleted!";
+    }
+}
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,30 @@
+package com.example.demo.services;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+    public UserDTO saveUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public List<UserDTO> getAllUsers() {
+        List<User>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDTO>>() {}.getType());
+    }
+}
