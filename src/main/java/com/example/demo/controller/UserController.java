f506078f3a5c9bcc24615e309b6428fa97829baf
mlahiru1 <164457106+mlahiru1@users.noreply.github.com>
2024-08-23 05:07:26 +0530

    deleteUser

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
     }
 
     @DeleteMapping("/deleteUser")
-    public String deleteUser(){
-        return "User Deleted!";
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
     }
 }
